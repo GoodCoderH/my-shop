9fcc49186fb77cdafa7215987fa45e6fb73413f9
GoodCoderH <77324409+GoodCoderH@users.noreply.github.com>
2022-04-29 21:56:52 +0900

    init front

--- front-end/src/routes/Home.js
+++
@@ -1,9 +0,0 @@
-import { useEffect, useState } from "react";
-import customAxios from "../api/customAxios";
-import Navbar from "../components/Navbar";
-
-function Home() {
-  return <Navbar />;
-}
-
-export default Home;
