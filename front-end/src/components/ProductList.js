cee5a43b5a5e9bb7fd05d944c57bea575eb382b8
GoodCoderH <77324409+GoodCoderH@users.noreply.github.com>
2022-06-01 21:21:48 +0900

    Jwt #14

--- front-end/src/components/ProductList.js
+++ front-end/src/components/ProductList.js
@@ -1,13 +1,22 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "../api/axios";
 
 function ProductList() {
-  useEffect(() => {
-    axios
-      .get("/products")
-      .then(console.log("Success"))
-      .catch(console.log("Failed"));
-  }, []);
-  return "hello";
+  const [products, setProducts] = useState();
+
+  return (
+    <article>
+      <h2>Product List</h2>
+      {products?.length ? (
+        <ul>
+          {products.map((product, i) => (
+            <li key={i}>{product?.name}</li>
+          ))}
+        </ul>
+      ) : (
+        <p>No products to display</p>
+      )}
+    </article>
+  );
 }
 export default ProductList;
